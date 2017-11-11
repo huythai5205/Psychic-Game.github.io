07bf5b935491ecc21424e3b205924fbb6816549c
Huy <kennyt995@gmail.com>
2017-11-11 10:56:49 -0700

    completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guess = 0;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+var letter = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    alert("User guess: " + userGuess);
+    if (userGuess === letter) {
+        wins++;
+        guessesLeft = 10;
+        letter = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        displayGame();
+    } else {
+        if (guessesLeft > 0) {
+            guessesLeft--;
+            guesses++;
+            displayGame();
+        } else {
+            losses++;
+            guessesLeft = 10;
+            letter = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+            displayGame();
+        }
+    }
+}
+
+function displayGame() {
+    document.getElementById("game").innerHTML = "<h1>The Psychic Game</h1><h3>Guess what letter I'm thinking of</h3><h3>Losses: " + wins + "</h3><h3>Losses: " + losses + "</h3><h3>Guesses left: " + guessesLeft + "</h3><h3>Your guesses so far: " + guesses + "</h3>";
+}
